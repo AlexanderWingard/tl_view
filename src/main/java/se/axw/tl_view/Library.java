01c7f5190abc4b9126c6db8fa7348538cd631d17
Alexander Wingård <alexander.wingard@gmail.com>
2016-11-21 02:55:06 +0100

    Gradle to the grave

---
+++ src/main/java/se/axw/tl_view/Library.java
@@ -0,0 +1,7 @@
+package se.axw.tl_view;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ src/test/java/se/axw/tl_view/LibraryTest.java
@@ -0,0 +1,13 @@
+package se.axw.tl_view;
+import org.junit.Test;
+
+import se.axw.tl_view.Library;
+
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
